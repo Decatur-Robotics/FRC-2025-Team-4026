561a2b2f13a58ff2db0e6504a698ff8c59159d69
thatonemaybe <97747093+seankid@users.noreply.github.com>
2025-01-24 17:15:29 -0500

    arm is no longer a part of claw motor in ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -6,7 +6,7 @@ public final class Ports {
     public static final int ELEVATOR_MOTOR_RIGHT = 17;
     public static final int CLIMBER_MOTOR_LEFT = 40;
     public static final int CLIMBER_MOTOR_RIGHT = 26;
-    public static final int ARM_CLAW_MOTOR = 0;
+    public static final int CLAW_MOTOR = 0;
     public static final int INTAKE_MOTOR_LEFT = 0;
     public static final int INTAKE_MOTOR_RIGHT = 0;
     
