0482f8ef82d6d71de76a3fec9cbff13b1133081c
Charles Kennedy <82charkenn@csdecatur.net>
2025-02-22 16:47:31 -0500

    Added the port numbers for motors

--- src/main/java/frc/robot/constants/Ports.java
+++ src/main/java/frc/robot/constants/Ports.java
@@ -3,19 +3,19 @@ package frc.robot.constants;
 public class Ports {
 
 
-    public static final int ARM_MOTOR = 0;
+    public static final int ARM_MOTOR = 22;
 
-    public static final int ELEVATOR_MOTOR_LEFT = 0;
-    public static final int ELEVATOR_MOTOR_RIGHT = 0;
+    public static final int ELEVATOR_MOTOR_LEFT = 18;
+    public static final int ELEVATOR_MOTOR_RIGHT = 17;
 
-    public static final int CLIMBER_MOTOR = 0;
+    public static final int CLIMBER_MOTOR = 4;
 
-    public static final int CLAW_MOTOR = 0;
+    public static final int CLAW_MOTOR = 40;
 
-    public static final int INTAKE_MOTOR_LEFT = 0;
-    public static final int INTAKE_MOTOR_RIGHT = 0;
+    public static final int INTAKE_MOTOR_LEFT = 30;
+    public static final int INTAKE_MOTOR_RIGHT = 31;
 
-    public static final int WRIST_MOTOR = 0;
+    public static final int WRIST_MOTOR = 39;
 
     public static final int ADDRESSABLE_LED = 0;
 
