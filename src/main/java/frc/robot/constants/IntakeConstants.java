b58b3c87bd030419eba7436fc3894d116a52c994
Owen Beck <oebeck@gmail.com>
2025-03-06 17:14:05 -0500

    Testing values for intake debouncer

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -18,8 +18,8 @@ public class IntakeConstants {
     public static final double PROCESSOR_EJECT_VELOCITY = 80;
     public static final double NET_EJECT_VELOCITY = 80;
 
-    public static final double STALL_DEBOUNCE_TIME = 10;
-    public static final int STALL_CURRENT = 0;
+    public static final double STALL_DEBOUNCE_TIME = 0.5;
+    public static final int STALL_CURRENT = 15;
 
     public static final CurrentLimitsConfigs CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs()
         .withStatorCurrentLimitEnable(true)
