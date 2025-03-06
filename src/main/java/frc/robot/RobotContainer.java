6ebb491fa6e35de931c96812dcd54887d6de5efc
Owen Beck <oebeck@gmail.com>
2025-03-06 17:39:50 -0500

    manual climber commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -180,6 +180,8 @@ public class RobotContainer {
         x.whileTrue(superstructure.intakeAlgaeReefCommand(getTargetPose));
 
         // bumperRight.whileTrue(climber.climbCommand());
+        bumperLeft.whileTrue(climber.setVoltageCommand(12));
+        bumperRight.whileTrue(climber.setVoltageCommand(-12));
 
         // bumperLeft.onTrue(superstructure.zeroSuperstructureCommand());
 
