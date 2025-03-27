df97784723eff38db646344c74e4ae4f50f35577
Owen Beck <oebeck@gmail.com>
2025-03-27 18:37:42 -0500

    Alignment error tuning

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -45,12 +45,12 @@ public class SwerveConstants {
     public static final double TRANSLATIONAL_AUTO_DEADBAND = 0; // MAX_TRANSLATIONAL_VELOCITY * 0.01;
     public static final double ROTATIONAL_AUTO_DEADBAND = 0; // MAX_ROTATIONAL_VELOCITY * 0.01;
 
-    public static final double TRANSLATIONAL_ALIGNED_ERROR_MARGIN = 0.035;
+    public static final double TRANSLATIONAL_ALIGNED_ERROR_MARGIN = 0.025;
     public static final double ROTATIONAL_ALIGNED_ERROR_MARGIN = 0.02;
     public static final double VELOCITY_ALIGNED_ERROR_MARGIN = 0.1;
 
     
-    public static final double TRANSLATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.3;
+    public static final double TRANSLATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.5;
     public static final double ROTATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.25;
     public static final double VELOCITY_NEAR_ALIGNED_ERROR_MARGIN = 0.5;
 
