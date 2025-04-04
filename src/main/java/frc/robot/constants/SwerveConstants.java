2bbbde529f6e91c6750fa3dc85b9ae0ebf64d1c6
Owen Beck <oebeck@gmail.com>
2025-04-04 16:29:34 -0500

    Line up tuning

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -48,13 +48,13 @@ public class SwerveConstants {
     public static final double ROTATIONAL_AUTO_DEADBAND = 0; // MAX_ROTATIONAL_VELOCITY * 0.01;
 
     public static final double TRANSLATIONAL_ALIGNED_ERROR_MARGIN = 0.025;
-    public static final double ROTATIONAL_ALIGNED_ERROR_MARGIN = 0.02;
+    public static final double ROTATIONAL_ALIGNED_ERROR_MARGIN = 0.03;
     public static final double VELOCITY_ALIGNED_ERROR_MARGIN = 0.1;
 
     
-    public static final double TRANSLATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.5;
+    public static final double TRANSLATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.75;
     public static final double ROTATIONAL_NEAR_ALIGNED_ERROR_MARGIN = 0.25;
-    public static final double VELOCITY_NEAR_ALIGNED_ERROR_MARGIN = 1;
+    public static final double VELOCITY_NEAR_ALIGNED_ERROR_MARGIN = 1.5;
 
     public static final double PRECISION_MODE_SCALAR = 0.25;
 
